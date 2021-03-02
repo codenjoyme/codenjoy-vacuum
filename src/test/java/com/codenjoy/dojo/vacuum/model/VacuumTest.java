95aa310848d86d8ce55c68ad09ec11964fdb47bf
Valentin Chizhov <valentin.chizhov@gmail.com>
2021-03-02 07:38:57 +0300

    Separate tests and create abstract test class which contains most of a boilerplate preparations

--- src/test/java/com/codenjoy/dojo/vacuum/model/VacuumTest.java
+++ src/test/java/com/codenjoy/dojo/vacuum/model/VacuumTest.java
@@ -1,86 +1,20 @@
 package com.codenjoy.dojo.vacuum.model;
 
-/*-
- * #%L
- * Codenjoy - it's a dojo-like platform from developers to developers.
- * %%
- * Copyright (C) 2018 Codenjoy
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
-
-
-import com.codenjoy.dojo.services.Dice;
-import com.codenjoy.dojo.services.EventListener;
-import com.codenjoy.dojo.services.printer.PrinterFactory;
-import com.codenjoy.dojo.services.printer.PrinterFactoryImpl;
-import com.codenjoy.dojo.utils.TestUtils;
-import com.codenjoy.dojo.vacuum.model.level.Level;
 import com.codenjoy.dojo.vacuum.services.Event;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.List;
 import java.util.Objects;
 
 import static com.codenjoy.dojo.vacuum.services.Event.*;
+import static com.codenjoy.dojo.vacuum.services.Event.DUST_CLEANED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-public class VacuumGameTest {
-
-    private VacuumGame game;
-    private Hero hero;
-    private Dice dice;
-    private EventListener listener;
-    private Player player;
-    private PrinterFactory printer = new PrinterFactoryImpl();
-
-    @Before
-    public void setup() {
-        dice = mock(Dice.class);
-    }
-
-    private void dice(int... ints) {
-        OngoingStubbing<Integer> when = when(dice.next(anyInt()));
-        for (int i : ints) {
-            when = when.thenReturn(i);
-        }
-    }
-
-    private void givenFl(String board) {
-        Level level = Level.generate(board);
-
-        game = new VacuumGame(level);
-        listener = mock(EventListener.class);
-        player = new Player(listener);
-        game.newGame(player);
-        player.newHero(game);
-        player.hero.init(game);
-        this.hero = game.getHeroes().get(0);
-    }
-
-    private void assertE(String expected) {
-        assertEquals(TestUtils.injectN(expected),
-                printer.getPrinter(game.reader(), player).print());
-    }
+public class VacuumTest extends AbstractGameTest {
 
     @Test
     public void shouldSpawnOnStartPoint() {
