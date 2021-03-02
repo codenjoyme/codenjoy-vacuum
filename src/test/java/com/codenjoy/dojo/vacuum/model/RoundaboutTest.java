c5068a13552c46cc57aa01af6c838e27a95d0551
Valentin Chizhov <valentin.chizhov@gmail.com>
2021-03-02 13:59:32 +0300

    Roundabout tests added

--- src/test/java/com/codenjoy/dojo/vacuum/model/RoundaboutTest.java
+++ src/test/java/com/codenjoy/dojo/vacuum/model/RoundaboutTest.java
@@ -1,6 +1,10 @@
 package com.codenjoy.dojo.vacuum.model;
 
+import com.codenjoy.dojo.vacuum.services.Event;
 import org.junit.Test;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 public class RoundaboutTest extends AbstractGameTest {
 
@@ -104,6 +108,236 @@ public class RoundaboutTest extends AbstractGameTest {
         assertE("#####" +
                 "# S #" +
                 "# O #" +
+                "#   #" +
+                "#####");
+    }
+
+    @Test
+    public void shouldNotBeFined_whenSandingOnRoundabout() {
+        givenFl("#####" +
+                "#   #" +
+                "# ┌S#" +
+                "#   #" +
+                "#####");
+
+        hero.left();
+        game.tick();
+
+        verify(listener, never()).event(Event.TIME_WASTED);
+        givenFl("#####" +
+                "#   #" +
+                "# ┐ #" +
+                "# S #" +
+                "#####");
+
+        hero.up();
+        game.tick();
+
+        verify(listener, never()).event(Event.TIME_WASTED);
+        givenFl("#####" +
+                "#   #" +
+                "#S┘ #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        verify(listener, never()).event(Event.TIME_WASTED);
+
+        givenFl("#####" +
+                "# S #" +
+                "# └ #" +
+                "#   #" +
+                "#####");
+
+        hero.down();
+        game.tick();
+        verify(listener, never()).event(Event.TIME_WASTED);
+    }
+
+    @Test
+    public void shouldNotBeAbleToEnterFromProhibitedSide() {
+        givenFl("#####" +
+                "#   #" +
+                "#S└ #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "#O└ #" +
+                "#   #" +
+                "#####");
+
+
+        givenFl("#####" +
+                "#   #" +
+                "#S┌ #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "#O┌ #" +
+                "#   #" +
+                "#####");
+
+        givenFl("#####" +
+                "# S #" +
+                "# ┐ #" +
+                "#   #" +
+                "#####");
+
+        hero.down();
+        game.tick();
+        assertE("#####" +
+                "# O #" +
+                "# ┐ #" +
+                "#   #" +
+                "#####");
+
+        givenFl("#####" +
+                "#   #" +
+                "# ┘S#" +
+                "#   #" +
+                "#####");
+
+        hero.left();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "# ┘O#" +
+                "#   #" +
+                "#####");
+    }
+
+    @Test
+    public void shouldBeAbleToEnterFromPermittedSide() {
+        // UP_RIGHT
+        givenFl("#####" +
+                "# S #" +
+                "# └ #" +
+                "#   #" +
+                "#####");
+
+        hero.down();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "# S #" +
+                "# ┌O#" +
+                "#   #" +
+                "#####");
+
+        givenFl("#####" +
+                "#   #" +
+                "# └S#" +
+                "#   #" +
+                "#####");
+
+        hero.left();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "# O #" +
+                "# ┌S#" +
+                "#   #" +
+                "#####");
+
+        // RIGHT_DOWN
+        givenFl("#####" +
+                "#   #" +
+                "# ┌S#" +
+                "#   #" +
+                "#####");
+
+        hero.left();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "# ┐S#" +
+                "# O #" +
+                "#####");
+
+        givenFl("#####" +
+                "#   #" +
+                "# ┌ #" +
+                "# S #" +
+                "#####");
+
+        hero.up();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "# ┐O#" +
+                "# S #" +
+                "#####");
+
+        // DOWN_LEFT
+        givenFl("#####" +
+                "#   #" +
+                "# ┐ #" +
+                "# S #" +
+                "#####");
+
+        hero.up();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "#O┘ #" +
+                "# S #" +
+                "#####");
+
+        givenFl("#####" +
+                "#   #" +
+                "#S┐ #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "#   #" +
+                "#S┘ #" +
+                "# O #" +
+                "#####");
+
+        // LEFT_UP
+        givenFl("#####" +
+                "#   #" +
+                "#S┘ #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "# O #" +
+                "#S└ #" +
+                "#   #" +
+                "#####");
+
+        givenFl("#####" +
+                "# S #" +
+                "# ┘ #" +
+                "#   #" +
+                "#####");
+
+        hero.down();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "# S #" +
+                "#O└ #" +
                 "#   #" +
                 "#####");
     }
