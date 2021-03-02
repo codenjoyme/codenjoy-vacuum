700f4b5b35134bda622bc149e8b87b3c7646b114
Valentin Chizhov <valentin.chizhov@gmail.com>
2021-03-02 09:30:40 +0300

    Direction switcher turn around test

--- src/test/java/com/codenjoy/dojo/vacuum/model/DirectionSwitcherTest.java
+++ src/test/java/com/codenjoy/dojo/vacuum/model/DirectionSwitcherTest.java
@@ -1,6 +1,10 @@
 package com.codenjoy.dojo.vacuum.model;
 
+import com.codenjoy.dojo.vacuum.services.Event;
 import org.junit.Test;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 public class DirectionSwitcherTest extends AbstractGameTest {
 
@@ -49,5 +53,58 @@ public class DirectionSwitcherTest extends AbstractGameTest {
                 "# ↑← #" +
                 "#    #" +
                 "######");
+    }
+
+    @Test
+    public void shouldTurnAround_whenStepOnSwitcherInOppositeDirection() {
+        givenFl("######" +
+                "#S   #" +
+                "#    #" +
+                "#    #" +
+                "#↑   #" +
+                "######");
+
+        hero.down();
+        game.tick();
+        game.tick();
+        game.tick();
+        assertE("######" +
+                "#S   #" +
+                "#    #" +
+                "#    #" +
+                "#O   #" +
+                "######");
+
+        game.tick();
+        game.tick();
+        game.tick();
+        assertE("######" +
+                "#O   #" +
+                "#    #" +
+                "#    #" +
+                "#↑   #" +
+                "######");
+    }
+
+    @Test
+    public void shouldNotBeFined_whenStepOnSwitcher() {
+        givenFl("#####" +
+                "#S↓ #" +
+                "#↑← #" +
+                "#   #" +
+                "#####");
+
+        hero.right();
+        game.tick();
+        game.tick();
+        game.tick();
+        game.tick();
+        assertE("#####" +
+                "#O↓ #" +
+                "#↑← #" +
+                "#   #" +
+                "#####");
+
+        verify(listener, never()).event(Event.TIME_WASTED);
     }
 }
